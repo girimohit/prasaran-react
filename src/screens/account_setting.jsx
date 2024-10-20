2a7f9d90e1452748686c9b9360a648f5d7faa101
Mohit Giri <mohitgiri1103@gmail.com>
2024-10-20 14:21:28 +0530

    Add Account settings screen and edit profile page

--- src/screens/account_setting.jsx
+++ src/screens/account_setting.jsx
@@ -19,7 +19,50 @@ const SettingOptions = ({ option_name, IconComponent }) => {
 const AccountSettingScreen = () => {
   return (
     <>
-      <p>Account Settings</p>
+      <div className="mb-8 flex flex-col items-center justify-center">
+        <h2 className="my-10 text-center font-bold">Account</h2>
+        <div className="h-32 w-32 rounded-full border-2 border-black bg-gray-50"></div>
+        <h3 className="font-semibold">Full Name</h3>
+        <p className="text-gray-400">@username</p>
+        <button
+          className="rounded-lg bg-black px-5 py-1 text-white"
+          onClick={"/edit-profile"}
+        >
+          Edit Profile
+        </button>
+      </div>
+
+      <hr className="mx-5 my-1 h-1 border-0 bg-gray-300" />
+
+      <div id="setting-options" className="mx-6">
+        <SettingOptions
+          option_name="Settings"
+          IconComponent={<IoSettingsOutline />}
+        />
+        <SettingOptions
+          option_name="My Activity"
+          IconComponent={<FaRegClipboard />}
+        />
+        <SettingOptions
+          option_name="Change Passcode"
+          IconComponent={<MdOutlineLock />}
+        />
+        <SettingOptions
+          option_name="Saved Posts"
+          IconComponent={<FaRegSave />}
+        />
+      </div>
+
+      <hr className="mx-5 my-1 h-1 border-0 bg-gray-300" />
+
+      <div id="setting-options" className="mx-6">
+        <SettingOptions
+          option_name="Help & Support"
+          IconComponent={<MdHelpOutline />}
+        />
+        <SettingOptions option_name="Log out" IconComponent={<MdLogout />} />
+      </div>
+      <div></div>
     </>
   );
 };
--- src/screens/edit_profile.jsx
+++ src/screens/edit_profile.jsx
@@ -12,7 +12,28 @@ const EditField = ({ field_name, field_data }) => {
 const EditProfileScreen = () => {
   return (
     <>
-      <p>Edit Profie Screen</p>
+      <div className="mb-8 flex flex-col items-center justify-center">
+        <h2 className="my-10 text-center font-bold">Edit Profile</h2>
+        <div className="mb-6 h-32 w-32 rounded-full border-2 border-black bg-gray-50"></div>
+        <h3 className="font-semibold">Full Name</h3>
+        <p className="text-gray-400">@username</p>
+      </div>
+
+      <hr className="mx-5 my-1 h-1 border-0 bg-gray-300" />
+
+      <div id="setting-options" className="mx-6 grid grid-cols-2 gap-x-3">
+        <EditField field_name="Username" field_data={"username"} />
+        <EditField field_name="Gender" field_data={"Male"} />
+        <EditField field_name="Date of Birth" field_data={"12/12/1999"} />
+        <EditField field_name="Full Name" field_data={"John Wick"} />
+        <EditField field_name="Email" field_data={"johnwick@gmail.com"} />
+      </div>
+
+      <div className="fixed bottom-4 left-0 right-0 flex justify-center">
+        <button className="w-96 rounded-xl bg-black px-5 py-2 text-white">
+          Save
+        </button>
+      </div>
     </>
   );
 };
