3c2875d029c35104fe5fe7b0a016306a9bca2338
Nimish Kumar <nimishkumar0305@gmail.com>
2024-10-22 04:04:57 +0530

    Add Page Nav

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Register from "./pages/Register";
 import LoginPage from './pages/loginPage';
+import Home from './pages/Home';
 
 function App() {
 
   return (
     <Router>
       <Routes>
-        {/* <Route path="/" element={<Home />} /> */}
+        <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<LoginPage />} />
       </Routes>
